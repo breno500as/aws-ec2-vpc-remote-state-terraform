a0d1a02a1f9647d08a5525e5df3744d55aca6d41
breno500as <breno500as@gmail.com>
2024-01-12 17:59:39 -0300

    init

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+ 
+
+
+resource "aws_key_pair" "key" {
+  key_name           = "aws-public-key"
+  public_key = file("./ec2-key.pub")
+}
+
+ 
+
+resource "aws_instance" "ec2" {
+  ami                         = "ami-0c7217cdde317cfec"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "ec2-terraform"
+  }
+
+}
